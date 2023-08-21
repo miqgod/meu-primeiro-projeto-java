286a5f8ac280a25b9847e7b80009ea4beb1cda2e
miqgod <mique.santoss@gmail.com>
2023-08-21 13:27:08 -0300

    teste

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boas vindas guerreiros!");
+        System.out.println("Boas vindas guerreiross");
     }
 }
